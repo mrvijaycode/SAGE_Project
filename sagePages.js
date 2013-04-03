6fc2236873a59659e9f394f1adbc077ec6cf8639
vijaymca <vijaymca@sify.com>
2013-04-03 19:52:07 +0530

    All working fine

--- sagePages.js
+++ sagePages.js
@@ -8,7 +8,6 @@
 //* Author           : Vijay Bhaskar C
 //****************************************************************
 
-
 var itmid = window.location.search.split("?itmid=")[1];
 
 var keyBodyArr = new Array();
@@ -17,6 +16,7 @@ var keyIDArr = new Array();
 
 $(document).ready(function () {
 	contentLoad();
+	toolsCapabilities();
 	//alert(itmid);
 });
 
@@ -60,6 +60,57 @@ function buidTable(strTitleArr, strBodyArr) {
 		}
 	});
 
-	var strHtml = "<h4>" + strTitle + "</h4><p>" + strBody + "</p>";
+	var strHtml = "<h4 style='padding-left:25px'>" + strTitle + "</h4><p>" + strBody + "</p>";
 	$("#divContent").html(strHtml)
+}
+
+function toolsCapabilities() {
+	var keyCapability = "";
+
+	switch (itmid) {
+	case "1":
+		keyCapability = "Store &amp; Shopper Intelligence";
+		break;
+
+	case "2":
+		keyCapability = "Shopper Sources of Growth";
+		break;
+
+	case "3":
+		keyCapability = "More Stores";
+		break;
+
+	case "4":
+		keyCapability = "Better Stores";
+		break;
+
+	case "5":
+		keyCapability = "Bigger Baskets";
+		break;
+
+	case "6":
+		keyCapability = "Superb Store Operations";
+		break;
+	}
+
+	var strHtml="<table><tr><td style='font-weight:bold'>Tools & Capabilities</td></tr>";
+	$().SPServices({
+		operation : "GetListItems",
+		async : false,
+		listName : "Tools &amp; Capabilities",
+		//CAMLQueryOptions : "<QueryOptions><IncludeAttachmentUrls>TRUE</IncludeAttachmentUrls></QueryOptions>",
+		CAMLQuery : "<Query><Where><Eq><FieldRef Name='Key_x0020_Measure' /><Value Type='Lookup'>" + keyCapability + "</Value></Eq></Where><OrderBy><FieldRef Name='Modified' /></OrderBy></Query>",
+		completefunc : function (xData, Status) {
+
+			//alert(xData.responseText);
+			$(xData.responseXML).SPFilterNode("z:row").each(function () {
+					var strURL = $(this).attr("ows_URL");
+					
+					strHtml+="<tr><td><a href='"+strURL.split(',')[0]+"'>"+strURL.split(',')[1]+"</a></td></tr>";
+					//alert(strHtml);
+				});
+		}
+	});
+	strHtml+="</table>";
+	$("#divLinks").html(strHtml);
 }
