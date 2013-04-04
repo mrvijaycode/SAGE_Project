c8dc644d951abff8ed5c2d62082c9ac3035f02ba
vijaymca <vijaymca@sify.com>
2013-04-04 16:13:34 +0530

    new design added

--- sagePages.js
+++ sagePages.js
@@ -66,34 +66,44 @@ function buidTable(strTitleArr, strBodyArr) {
 
 function toolsCapabilities() {
 	var keyCapability = "";
+	var newImg = "http://teamspace.pg.com/sites/ShopperBasedDesign/SAGE/WPPages/Images/";
 
 	switch (itmid) {
 	case "1":
 		keyCapability = "Store &amp; Shopper Intelligence";
+		newImg += "store_shopper_intelligence.png";
 		break;
 
 	case "2":
 		keyCapability = "Shopper Sources of Growth";
+		newImg += "shoppersource_of_growth.png";
 		break;
 
 	case "3":
 		keyCapability = "More Stores";
+		newImg += "more_stores.png";
 		break;
 
 	case "4":
 		keyCapability = "Better Stores";
+		newImg += "better_stores.png";
 		break;
 
 	case "5":
 		keyCapability = "Bigger Baskets";
+		newImg += "bigger_basket.png";
 		break;
 
 	case "6":
 		keyCapability = "Superb Store Operations";
+		newImg += "superb_store_operations.png";
 		break;
 	}
 
-	var strHtml="<table><tr><td style='font-weight:bold'>Tools & Capabilities</td></tr>";
+	$("#spnHeader").html(keyCapability);
+	$("#imgHeader").attr("src", newImg)
+
+	var strHtml = "<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"5\">";
 	$().SPServices({
 		operation : "GetListItems",
 		async : false,
@@ -104,13 +114,13 @@ function toolsCapabilities() {
 
 			//alert(xData.responseText);
 			$(xData.responseXML).SPFilterNode("z:row").each(function () {
-					var strURL = $(this).attr("ows_URL");
-					
-					strHtml+="<tr><td><a href='"+strURL.split(',')[0]+"'>"+strURL.split(',')[1]+"</a></td></tr>";
-					//alert(strHtml);
-				});
+				var strURL = $(this).attr("ows_URL");
+
+				strHtml += "<tr><td><img src='http://teamspace.pg.com/sites/ShopperBasedDesign/SAGE/WPPages/Images/links_bullet.jpg' /><a style='font-size:13px' href='" + strURL.split(',')[0] + "'>" + strURL.split(',')[1] + "</a></td></tr>";
+				//alert(strHtml);
+			});
 		}
 	});
-	strHtml+="</table>";
+	strHtml += "</table>";
 	$("#divLinks").html(strHtml);
 }
